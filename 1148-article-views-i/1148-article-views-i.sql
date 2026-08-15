d1b722c496136379d9fde36f7ffc06f949839491
dishant Garg <dishantgarg050@gmailo.com>
2026-08-15 20:26:50 +0530

    Time: 524 ms (18.05%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+-- table:VIEWS
+-- | article_id | author_id | viewer_id | view_date  |
+-- +------------+-----------+-----------+------------+
+-- | 1          | 3         | 5         | 2019-08-01 |
+-- | 1          | 3         | 6         | 2019-08-02 |
+-- | 2          | 7         | 7         | 2019-08-01 |
+-- | 2          | 7         | 6         | 2019-08-02 |
+-- | 4          | 7         | 1         | 2019-07-22 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+
+-- FIND all the author that viewd atleast one of their own article
+(select distinct author_id as id  from views where author_id=viewer_id)order by id;
