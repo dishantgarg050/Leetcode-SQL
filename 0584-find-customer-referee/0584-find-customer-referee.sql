9b3193419d81963a7c27809a7f6004bd6fefddea
dishant Garg <dishantgarg050@gmailo.com>
2026-08-15 19:33:08 +0530

    Time: 550 ms (36.26%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+-- table-CUSTOMER
+-- id   name    refree_id
+-- 1              will    null
+-- 2              jane    null
+-- 3              alex    2
+-- 4              bill    null
+-- 5              zack    1
+-- 6              mark    2
+
+-- find the name of customer referred by any customer with id!=2  
+-- and not refeered by any cuctomer
+select name  from customer where referee_id !=2 or referee_id is null;
+
