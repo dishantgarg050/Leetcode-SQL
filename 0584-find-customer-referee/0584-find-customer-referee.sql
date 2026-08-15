71a8f3772b4649008ace6720294478f8d16bd3f8
dishant Garg <dishantgarg050@gmailo.com>
2026-08-15 19:57:43 +0530

    Fix SQL comments and update query logic
    Corrected the comment formatting and clarified the SQL query to find customers not referred by customer with id 2.

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 -- table-CUSTOMER
 -- id   name    refree_id
--- 1              will    null
--- 2              jane    null
--- 3              alex    2
--- 4              bill    null
--- 5              zack    1
--- 6              mark    2
+-- 1    will    null
+-- 2    jane    null
+-- 3    alex    2
+-- 4    bill    null
+-- 5    zack    1
+-- 6    mark    2
 
 -- find the name of customer referred by any customer with id!=2  
 -- and not refeered by any cuctomer
