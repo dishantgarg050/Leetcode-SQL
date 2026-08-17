42ba153a99544cdac33223ee1232fc2d1185951e
dishant Garg <dishantgarg050@gmailo.com>
2026-08-18 02:05:47 +0530

    Time: 489 ms (13.64%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -19,3 +19,41 @@ ROUND((select count(query_name) from queries where rating<3 AND
 query_name = q.query_name)*100/
 (select count(query_name) from queries where query_name = q.query_name), 2) as poor_query_percentage 
 from queries as q  group by query_name;
+-- (both inner references outer query )
+-- both subquery use q.queryname as 
+
+
+-- DRY RUN-CO-RELATED SUBQUERY
+-- STEP 1: FROM Queries AS q
+
+-- STEP 2: GROUP BY query_name
+-- GROUP "Dog" - Row1, Row2, Row3
+-- GROUP "Cat" - Row4, Row5, Row6
+-- (Ab ek group ko process karega, phir doosre ko - one at a time.)
+
+-- STEP 3: group "Dog" (q.query_name = 'Dog')
+--  -->quality = ROUND(AVG(rating/position), 2)=2.50
+-- (Numerator subquery: WHERE rating < 3 AND query_name = 'Dog'
+-- Row1: rating=5, <3?-NO
+-- Row2: rating=5, <3?-NO
+-- Row3: rating=1, <3?-YES-->count=1
+-- Denominator subquery: WHERE query_name = 'Dog'
+-- Row1, Row2, Row3 --> count = 3)
+--  -->Calculate:poor_query_percentage = 1 *100 / 3 = 33.33
+-- SO, Dog output: ('Dog', 2.50, 33.33)
+
+-- STEP 4: group "Cat" (q.query_name = 'Cat')
+--  -->quality = ROUND(AVG(rating/position), 2)=0.66
+-- (Numerator subquery: WHERE rating < 3 AND query_name = 'Cat'
+-- Row1: rating=5, <3?-YES-->count=1
+-- Row2: rating=5, <3?-NO
+-- Row3: rating=1, <3?-NO 
+-- Denominator subquery: WHERE query_name = 'Cat'
+-- Row1, Row2, Row3 --> count = 3)
+--  -->Calculate:poor_query_percentage = 1 *100 / 3 = 33.33
+-- SO, Cat output: ('Cat', 0.66, 33.33)
+
+-- Final Output:
+-- query_name	quality	  poor_query_percentage
+--     Dog	    2.50	        33.33
+--     Cat	    0.66	        33.33
