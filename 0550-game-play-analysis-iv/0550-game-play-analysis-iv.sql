ae7a645e5a81b0e8dcee17dd5404888d697760a2
dishant Garg <dishantgarg050@gmailo.com>
2026-08-19 05:00:59 +0530

    Time: 3885 ms (5%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -11,10 +11,11 @@
 
 -- FIND THE fraction/percentage of players that logged in again on the day after the day they first logged in and  rounded to 2 decimal places.
 -- means  kitne playerse ne again login kiya usdin ke baad jb unhone initial login kiya tha
+
 SELECT ROUND(COUNT(a1.player_id) /(select COUNT(DISTINCT player_id) from activity), 2) AS fraction 
 FROM Activity a1
 WHERE DATEDIFF(a1.event_date, (SELECT MIN(a2.event_date)FROM Activity a2
                                WHERE a1.player_id = a2.player_id)
                 ) = 1;
-
+-- CO-RELATED SUBQUERY
              
