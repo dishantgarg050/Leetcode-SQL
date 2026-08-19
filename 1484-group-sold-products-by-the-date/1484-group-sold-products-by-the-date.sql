d09190555036ff1aac822c7e17de520124e131ff
dishant Garg <dishantgarg050@gmailo.com>
2026-08-20 00:41:34 +0530

    Time: 550 ms (24.6%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+-- table:ACTIVITIES
+-- | sell_date  | product     |
+-- +------------+------------+
+-- | 2020-05-30 | Headphone  |
+-- | 2020-06-01 | Pencil     |
+-- | 2020-06-02 | Mask       |
+-- | 2020-05-30 | Basketball |
+-- | 2020-06-01 | Bible      |
+-- | 2020-06-02 | Mask       |
+-- | 2020-05-30 | T-Shirt    |
+
+-- find for each date , the no of diiferent products sold and their names and  order by sell_date
+-- and sorted the sold product for each date lexiographically 
+select  sell_date, count(distinct product) as num_sold, 
+                   group_concat(distinct product) as products  
+ from activities
+ group by sell_date order by sell_date ,product;
+
+ 
+
+
