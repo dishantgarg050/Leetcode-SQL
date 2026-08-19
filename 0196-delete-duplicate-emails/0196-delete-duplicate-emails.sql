c6e8dc87f915187550b51d146902dc24bfe2fe04
dishant Garg <dishantgarg050@gmailo.com>
2026-08-19 12:32:15 +0530

    Time: 922 ms (46.2%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -7,5 +7,7 @@
 -- | 3  | john@example.com |
 
 -- find a solution to delete dupliocate emails, keep only unique email with smaalest id
+
 delete p2 from person p2 inner join person p1
-on p1.email=p2.email and p2.id>p1.id;
+on p1.email=p2.email and p2.id>p1.id;-- use SELF JOIN(INNER JOIN)
+-- DELETE P2 FROM "PERSON P1 INNER  PERSON P2"(RESULT TABLE)
