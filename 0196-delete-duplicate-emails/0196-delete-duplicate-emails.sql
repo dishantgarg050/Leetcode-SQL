b37ceafadfeb0a00b00f684baad414ea1656b461
dishant Garg <dishantgarg050@gmailo.com>
2026-08-19 12:25:54 +0530

    Time: 930 ms (43.78%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+-- table:PERSON
+-- |id(PK)| email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+
+-- find a solution to delete dupliocate emails, keep only unique email with smaalest id
+delete p2 from person p2 inner join person p1
+on p1.email=p2.email and p2.id>p1.id;
