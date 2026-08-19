c9153c50ed0429765453224a171e9ad5c8d504db
dishant Garg <dishantgarg050@gmailo.com>
2026-08-20 02:08:48 +0530

    Time: 989 ms (16.5%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -5,6 +5,15 @@ orders as o on p.product_id=o.product_id
              where  year(o.order_date)= 2020 and month(o.order_date)=2
 
 group by o.product_id  having unit>=100;
+-- dsolve step by step
+-- 1. join the table on condition
+-- 2. group by product_id
+-- 3.filter on group unit>=100
+-- 4. select (feb 2020) data from result table
+-- SO, FINAL OUTPUT: | product_name       | unit |
+--                   | ------------------ | ---- |
+--                   | Leetcode Solutions | 130  |
+--                   | Leetcode Kit       | 100  |
 
 
  
