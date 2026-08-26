53bc117ec96a578f6a6a31fb7cadbe67ebaf6575
dishant Garg <dishantgarg050@gmailo.com>
2026-08-27 00:02:46 +0530

    Time: 695 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,17 @@
 # Write your MySQL query statement below
 -- table-EMPLOYEE
+-- 1st case input
 -- |id(PK)| salary |
 -- +----+--------+
 -- | 1  | 100    |
 -- | 2  | 200    |
 -- | 3  | 300    |
+
+-- 2nd case input
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+
 
 -- FIND the second highest salary 
 -- select salary from employee order by salaru desc 
@@ -12,3 +19,4 @@
 select max(e1.salary) as SecondHighestSalary from employee e1 where 2=
 (select count(distinct e2.salary) from employee e2 
         where e2.salary>=e1.salary);
+-- 2nd case input only 100 h table me then count-->1!=2 then no second highest salary return null
